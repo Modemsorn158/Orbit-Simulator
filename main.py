b276b1ae994aa2426f42aa2fd6a29fe4f3c616b0
Modemsorn158 <mdytcha@gmail.com>
2026-08-25 09:27:01 +0700

    Compare Euler integrator trajectories

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from constants import EARTH_MU
 from integrators import forward_euler_step, semi_implicit_euler_step
 from simulation import simulate
-from plotter import plot_trajectory
+from plotter import plot_trajectory, plot_integrator_comparison
 from math import sqrt
 
 if __name__ == "__main__":
     # Figure 1: Simulate and plot the trajectory of a satellite in a circular orbit around Earth using forward Euler integration.
     r = 7000000
     vc = sqrt(EARTH_MU / r)  # m/s
-    positions = simulate(r, 0, 0, vc, 10, 600, forward_euler_step)
-    plot_trajectory(positions)
-    
+    positions_forward = simulate(r, 0, 0, vc, 10, 600, forward_euler_step)
+    plot_trajectory(positions_forward)
+
     # Figure 2: Simulate and plot the trajectory of a satellite in a circular orbit around Earth using semi-implicit Euler integration.
-    positions = simulate(r, 0, 0, vc, 10, 600, semi_implicit_euler_step)
-    plot_trajectory(positions)
+    positions_semi_implicit = simulate(r, 0, 0, vc, 10, 600, semi_implicit_euler_step)
+    plot_trajectory(positions_semi_implicit)
+
+    # Figure 3: Compare the trajectories of the two integrators.
+    plot_integrator_comparison(positions1=positions_forward, positions2=positions_semi_implicit, label1="Forward Euler", label2="Semi-Implicit Euler")
--- plotter.py
+++ plotter.py
@@ -15,3 +15,22 @@ def plot_trajectory(positions):
     plt.axis('equal')
     plt.grid()
     plt.show()
+    
+def plot_integrator_comparison(positions1, positions2, label1, label2):
+    """Plots the trajectories of two satellites for integrator comparison."""
+    
+    x_list1, y_list1 = zip(*positions1)
+    x_list2, y_list2 = zip(*positions2)
+    
+    plot, ax = plt.subplots()
+    ax.plot(x_list1, y_list1, linestyle='-', color='b', label=label1)
+    ax.plot(x_list2, y_list2, linestyle='-', color='r', label=label2)
+    earth_circle = plt.Circle((0, 0), EARTH_RADIUS, color='g', alpha=0.5)
+    ax.add_artist(earth_circle)
+    plt.title('Integrator Comparison')
+    plt.xlabel('X Position (m)')
+    plt.ylabel('Y Position (m)')
+    plt.axis('equal')
+    plt.grid()
+    plt.legend()
+    plt.show()
