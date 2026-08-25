823da1165f8852085108cd8dd855cd1bb8c787ec
Modemsorn158 <mdytcha@gmail.com>
2026-08-26 06:11:58 +0700

    Stop simulation on collision

--- simulation.py
+++ simulation.py
@@ -1,8 +1,19 @@
-def simulate(x, y, vx, vy, dt, steps, integration_step):
+def simulate(x, y, vx, vy, dt, steps, integration_step, collision_check=None):
     """Simulate the motion of an object under gravity for a given number of steps using specified integration method."""
     
-    states = [(x, y, vx, vy)]
+    if collision_check:
+        states = [(x, y, vx, vy)]
+        if collision_check(x, y):
+            return states
+    else:
+        states = [(x, y, vx, vy)]
     for i in range(steps):
         x, y, vx, vy = integration_step(x, y, vx, vy, dt)
-        states.append((x, y, vx, vy))
+        if collision_check:
+            states.append((x, y, vx, vy))
+            if collision_check(x, y):
+                return states
+        else:
+            states.append((x, y, vx, vy))
+        
     return states
--- tests/test_collisions.py
+++ tests/test_collisions.py
@@ -1,6 +1,9 @@
 import unittest
-from constants import EARTH_RADIUS
+from simulation import simulate
+from constants import EARTH_RADIUS, EARTH_MU
 from collision import has_collision_with_earth
+from integrators import velocity_verlet_step
+from math import sqrt
 
 class TestCollisions(unittest.TestCase):
     # Earth collisions
@@ -14,3 +17,15 @@ class TestCollisions(unittest.TestCase):
         self.assertTrue(c1)
         self.assertFalse(c2)
         self.assertTrue(c3)
+        
+    # Simulation test
+    def test_collision_simulation(self):
+        x1, y1 = EARTH_RADIUS, 0
+        x2, y2 = 7000000, 0
+        vc1 = sqrt(EARTH_MU / EARTH_RADIUS)
+        vc2 = sqrt(EARTH_MU / x2)
+        states1 = simulate(x1, y1, 0, vc1, 10, 500, velocity_verlet_step, has_collision_with_earth)
+        states2 = simulate(x2, y2, 0, vc2, 10, 500, velocity_verlet_step, has_collision_with_earth)
+        self.assertEqual(len(states1), 1)
+        self.assertEqual(len(states1[0]), 4)
+        self.assertEqual(len(states2), 501)
