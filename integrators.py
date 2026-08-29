5737427c127ed2443f1545919b59ce6905355be4
Modem158 <mdytcha@gmail.com>
2026-08-29 18:25:32 +0700

    Refactor validation to support data classes (Non-functioning build)

--- integrators.py
+++ integrators.py
@@ -5,11 +5,11 @@ def forward_euler_step(
     state: BodyState,
     dt: float,
     acceleration_model: Callable[[Vector2, BodyState], Vector2],
-    args: list
+    acceleration_args: list
 ) -> BodyState:
     """Perform a single time step using the Forward Euler method."""
     
-    a = acceleration_model(state.position, *args)
+    a = acceleration_model(state.position, *acceleration_args)
     position = state.position + (state.velocity * dt)
     velocity = state.velocity + (a * dt)
     return BodyState(
@@ -22,11 +22,11 @@ def semi_implicit_euler_step(
     state: BodyState,
     dt: float,
     acceleration_model: Callable[[Vector2, BodyState], Vector2],
-    args: list
+    acceleration_args: list
 ) -> BodyState:
     """Perform a single time step using the Semi-Implicit Euler method."""
     
-    a = acceleration_model(state.position, *args)
+    a = acceleration_model(state.position, *acceleration_args)
     velocity = state.velocity + (a * dt)
     position = state.position + (velocity * dt)
     return BodyState(
@@ -39,13 +39,13 @@ def velocity_verlet_step(
     state: BodyState,
     dt: float,
     acceleration_model: Callable[[Vector2, BodyState], Vector2],
-    args: list
+    acceleration_args: list
 ) -> BodyState:
     """Perform a single time step using the Velocity Verlet method."""
     
-    a = acceleration_model(state.position, *args)
+    a = acceleration_model(state.position, *acceleration_args)
     position = state.position + (state.velocity * dt) + (a * (0.5 * (dt ** 2)))
-    velocity = state.velocity + ((a + acceleration_model(position, *args)) * (0.5 * dt))
+    velocity = state.velocity + ((a + acceleration_model(position, *acceleration_args)) * (0.5 * dt))
     return BodyState(
         body = state.body,
         position = position,
--- validation.py
+++ validation.py
@@ -1,15 +1,39 @@
-from constants import EARTH_MU
+from typing import Callable
+from constants import GRAVITATIONAL_CONSTANT
 from simulation import simulate
 from diagnostics import specific_energy_history, relative_change_percent
+from gravity import gravitational_acceleration
+from state import *
 from math import sqrt
 
-def circular_orbit_max_energy_drift(radius, total_time, dt, integration_step):
+def circular_orbit_max_energy_drift(
+    state: BodyState,
+    source: BodyState,
+    dt: float,
+    total_time: int,
+    integration_step: Callable[[BodyState, float, Callable[[Vector2, BodyState], Vector2], list], BodyState]
+) -> float:
     """Simulate a circular orbit and return the maximum percentage energy drift over the simulation."""
     
     if (total_time % dt) != 0:
         raise ValueError("Total time must be an integer multiple of dt.")
-    vc = sqrt(EARTH_MU / radius)
-    states = simulate(radius, 0, 0, vc, dt, (total_time // dt), integration_step)
-    energy_history = specific_energy_history(states)
+    position = (state.position - source.position)
+    r = position.magnitude()
+    if r == 0:
+        raise ValueError("Position is at the origin of the other body.")
+    vc = sqrt((GRAVITATIONAL_CONSTANT * source.body.mass) / r)
+    unit_radius = position / r
+    vcx = -(unit_radius.y * vc)
+    vcy = unit_radius.x * vc
+    new_state = BodyState(
+        body = state.body,
+        position = state.position,
+        velocity = Vector2(
+            x = vcx + source.velocity.x,
+            y = vcy + source.velocity.y
+        )
+    )
+    states = simulate(new_state, dt, int(total_time // dt), integration_step, gravitational_acceleration, [source])
+    energy_history = specific_energy_history(states, source)
     relative_change = relative_change_percent(energy_history)
     return max(abs(change) for change in relative_change)
