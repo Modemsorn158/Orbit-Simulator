2919785043b06217c8d602d07a3a66491b7642c0
Modem158 <mdytcha@gmail.com>
2026-09-02 06:59:12 +0700

    Test 3-Body system

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,4 +1,3 @@
-from typing import final
 import unittest
 from constants import GRAVITATIONAL_CONSTANT
 from integrators import system_velocity_verlet_step
@@ -6,7 +5,7 @@ from gravity import system_gravitational_accelerations
 from simulation import simulate_system
 from system_diagnostics import total_linear_momentum, center_of_mass, total_angular_momentum, total_mechanical_energy
 from state import *
-from math import sqrt
+from math import sqrt, pi
 
 class TestSystem(unittest.TestCase):
     # Earth-Sun system simulation
@@ -96,6 +95,96 @@ class TestSystem(unittest.TestCase):
         self.assertTrue(earth.position.x < 0)
         self.assertTrue(len(system_states) == (steps + 1))
         self.assertTrue(final_system.time == (steps * dt))
+        self.assertAlmostEqual(linear_epsilon, 0)
+        self.assertAlmostEqual(center1.x, center2.x)
+        self.assertAlmostEqual(center1.y, center2.y)
+        self.assertAlmostEqual(angular_epsilon, 0)
+        self.assertAlmostEqual(energy_epsilon, 0)
+        
+    def test_equal_three_body(self):
+        body = Body(
+            name = "Planet",
+            mass = (10 ** 10),
+            radius = (10 ** 4)
+        )
+        r = 100000
+        s = r * sqrt(3)
+        omega = sqrt((GRAVITATIONAL_CONSTANT * body.mass) / (sqrt(3) * (r ** 3)))
+        planet1 = BodyState(
+            body = body,
+            position = Vector2(
+                x = r,
+                y = 0
+            ),
+            velocity = omega * Vector2(0, r)
+        )
+        planet2 = BodyState(
+            body = body,
+            position = Vector2(
+                x = -(r / 2),
+                y = (r / 2) * sqrt(3)
+            ),
+            velocity = omega * Vector2(-((r / 2) * sqrt(3)), -(r / 2))
+        )
+        planet3 = BodyState(
+            body = body,
+            position = Vector2(
+                x = -(r / 2),
+                y = -(r / 2) * sqrt(3)
+            ),
+            velocity = omega * Vector2(((r / 2) * sqrt(3)), -(r / 2))
+        )
+        system = SystemState(
+            body_states = (
+                planet1, planet2, planet3
+            ),
+            time = 0
+        )
+        T = (2 * pi) / omega
+        steps = 10000
+        dt = (T / steps)
+        system_states = simulate_system(system, dt, steps, system_velocity_verlet_step, system_gravitational_accelerations, [])
+        final_system = system_states[-1]
+        planet1_final = final_system.body_states[0]
+        planet2_final = final_system.body_states[1]
+        planet3_final = final_system.body_states[2]
+        linear1 = total_linear_momentum(system)
+        linear2 = total_linear_momentum(final_system)
+        linear_scale = 0
+        for state in final_system.body_states:
+            linear_scale = linear_scale + (state.body.mass * state.velocity.magnitude())
+        linear_epsilon = ((linear2 - linear1).magnitude() / linear_scale)
+        center1 = center_of_mass(system)
+        center2 = center_of_mass(final_system)
+        angular1 = total_angular_momentum(system)
+        angular2 = total_angular_momentum(final_system)
+        angular_scale = 0
+        for state in final_system.body_states:
+            angular_scale = angular_scale + (state.body.mass * ((state.position.x * state.velocity.y) - (state.position.y * state.velocity.x)))
+        angular_epsilon = ((angular2 - angular1) / angular_scale)
+        energy1 = total_mechanical_energy(system)
+        energy2 = total_mechanical_energy(final_system)
+        K = 0
+        U = 0
+        for i in range(len(final_system.body_states)):
+            state = final_system.body_states[i]
+            K = K + ((1 / 2) * (state.body.mass * (state.velocity.magnitude() ** 2)))
+            for j in range(len(final_system.body_states)):
+                if i < j:
+                    state2 = final_system.body_states[j]
+                    U = U + ((GRAVITATIONAL_CONSTANT * state.body.mass * state2.body.mass) / (state.position - state2.position).magnitude())
+        U = -U
+        E = K + U
+        energy_epsilon = ((energy2 - energy1) / E)
+        self.assertAlmostEqual(planet1.position.x, planet1_final.position.x, 0)
+        self.assertAlmostEqual(planet1.position.y, planet1_final.position.y, 0)
+        self.assertAlmostEqual(planet2.position.x, planet2_final.position.x, 0)
+        self.assertAlmostEqual(planet2.position.y, planet2_final.position.y, 0)
+        self.assertAlmostEqual(planet3.position.x, planet3_final.position.x, 0)
+        self.assertAlmostEqual(planet3.position.y, planet3_final.position.y, 0)
+        self.assertAlmostEqual((planet1_final.position - planet2_final.position).magnitude(), s)
+        self.assertAlmostEqual((planet1_final.position - planet3_final.position).magnitude(), s)
+        self.assertAlmostEqual((planet2_final.position - planet3_final.position).magnitude(), s)
         self.assertAlmostEqual(linear_epsilon, 0)
         self.assertAlmostEqual(center1.x, center2.x)
         self.assertAlmostEqual(center1.y, center2.y)
